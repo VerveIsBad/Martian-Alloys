f22907583724144921b462d631dafb4c4b7486b5
Payton <coatspayton14@gmail.com>
2021-08-30 12:54:16 +0000

    Added some runes Runes will be used for upgrading tools and such.

--- src/main/java/paytonthemartian/martian_alloys/MartianAlloysMod.java
+++ src/main/java/paytonthemartian/martian_alloys/MartianAlloysMod.java
@@ -7,9 +7,15 @@ public class MartianAlloysMod implements ModInitializer {
 	public static final Alloy AMETHYST_IRON_ALLOY = new Alloy("amethyst_iron", 800, 3.0F, 5.0F, 3, 15, null);
 	public static final Alloy OBSIDIAMOND_ALLOY = new Alloy("obsidiamond", 2000, 6.0F, 6.0F, 4, 25, null);
 
+	public static final Rune RUNE_EMPTY = new Rune(new FabricItemSettings().group(ItemGroup.TOOLS));
+	public static final Rune RUNE_FLIGHT = new Rune(new FabricItemSettings().group(ItemGroup.TOOLS));
+
 	@Override
 	public void onInitialize() {
 		Alloy.registerAlloy(AMETHYST_IRON_ALLOY);
 		Alloy.registerAlloy(OBSIDIAMOND_ALLOY);
+		
+		Registry.register(new Identifier("martian_alloys", "empty_rune"), RUNE_EMPTY);
+		Registry.register(new Identifier("martian_alloys", "flight_rune"), RUNE_FLIGHT);
 	}
 }
---
+++ src/main/java/paytonthemartian/martian_alloys/Rune.java
@@ -0,0 +1,9 @@
+package paytonthemartian.martian_alloys;
+
+import net.minecraft.item.Item;
+
+public class Rune extends Item {
+    public Rune(Settings settings) {
+        super(settings);
+    }
+}
