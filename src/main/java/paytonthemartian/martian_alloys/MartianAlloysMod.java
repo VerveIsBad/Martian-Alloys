31228e0c2487b072d5234d8c61fded8a3ef32ede
Payton <coatspayton14@gmail.com>
2021-08-30 15:43:31 +0000

    Fixed imports (again)

--- src/main/java/paytonthemartian/martian_alloys/MartianAlloysMod.java
+++ src/main/java/paytonthemartian/martian_alloys/MartianAlloysMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
 
 public class MartianAlloysMod implements ModInitializer {
 
