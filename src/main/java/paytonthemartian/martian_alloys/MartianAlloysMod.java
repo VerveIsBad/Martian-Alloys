892ff9d9b315f110d22f246f582a1c20124a2bf7
Payton <coatspayton14@gmail.com>
2021-08-30 15:29:27 +0000

    Added required import statements.

--- src/main/java/paytonthemartian/martian_alloys/MartianAlloysMod.java
+++ src/main/java/paytonthemartian/martian_alloys/MartianAlloysMod.java
@@ -1,6 +1,9 @@
 package paytonthemartian.martian_alloys;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.item.v1.FabricItemSettings;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.item.ItemGroup;
 
 public class MartianAlloysMod implements ModInitializer {
 
