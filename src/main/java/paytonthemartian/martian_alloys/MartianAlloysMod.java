8768b079fbc0dc7ad45e07aa0da022628cf897b4
Payton Coats <debeast591@gmail.com>
2021-09-01 07:39:02 -0400

    Added many more runes Still missing runes for about half of the enchants

--- src/main/java/paytonthemartian/martian_alloys/MartianAlloysMod.java
+++ src/main/java/paytonthemartian/martian_alloys/MartianAlloysMod.java
@@ -15,6 +15,16 @@ public class MartianAlloysMod implements ModInitializer {
 	public static final Item RUNE_SHARPNESS = new Rune(Enchantments.SHARPNESS);
 	public static final Item RUNE_FIRE_ASPECT = new Rune(Enchantments.FIRE_ASPECT);
 	public static final Item RUNE_EFFICIENCY = new Rune(Enchantments.EFFICIENCY);
+	public static final Item RUNE_AQUA_AFFINITY = new Rune(Enchantments.AQUA_AFFINITY);
+	public static final Item RUNE_BLAST_PROTECTION = new Rune(Enchantments.BLAST_PROTECTION);
+	public static final Item RUNE_DEPTH_STRIDER = new Rune(Enchantments.DEPTH_STRIDER);
+	public static final Item RUNE_FEATHER_FALLING = new Rune(Enchantments.FEATHER_FALLING);
+	public static final Item RUNE_FROST_WALKER = new Rune(Enchantments.FROST_WALKER);
+	public static final Item RUNE_PROJECTILE_PROTECTION = new Rune(Enchantments.PROJECTILE_PROTECTION);
+	public static final Item RUNE_PROTECTION = new Rune(Enchantments.PROTECTION);
+	public static final Item RUNE_RESPIRATION = new Rune(Enchantments.RESPIRATION);
+	public static final Item RUNE_SOUL_SPEED = new Rune(Enchantments.SOUL_SPEED);
+	public static final Item RUNE_THORNS = new Rune(Enchantments.THORNS);
 
 	@Override
 	public void onInitialize() {
@@ -25,5 +35,15 @@ public class MartianAlloysMod implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_sharpness"), RUNE_SHARPNESS);
 		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_fire_aspect"), RUNE_FIRE_ASPECT);
 		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_efficiency"), RUNE_EFFICIENCY);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_aqua_affinity"), RUNE_AQUA_AFFINITY);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_blast_protection"), RUNE_BLAST_PROTECTION);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_depth_strider"), RUNE_DEPTH_STRIDER);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_feather_falling"), RUNE_FEATHER_FALLING);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_frost_walker"), RUNE_FROST_WALKER);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_projectile_protection"), RUNE_PROJECTILE_PROTECTION);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_protection"), RUNE_PROTECTION);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_respiration"), RUNE_RESPIRATION);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_soul_speed"), RUNE_SOUL_SPEED);
+		Registry.register(Registry.ITEM, new Identifier("martian_alloys", "rune_thorns"), RUNE_THORNS);
 	}
 }
